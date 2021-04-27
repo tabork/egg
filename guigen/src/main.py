f4a79fba88d015efd4c5ebe8f8ba930e589bd358
Tabor Kvanicka <kamakwazee@gmail.com>
2021-04-27 11:24:03 -0500

    Cleaned up sim

--- guigen/src/main.py
+++ guigen/src/main.py
@@ -1,60 +1,23 @@
-# from guigen.simulator import Simulator
-# from guigen.generator import Generator
-from ga.structures import UIRepr
-
-from ga.ga import GA
+from guigen.simulator import Simulator
 
 import os
 import random
 import sys
+import time
 
 
 if __name__ == "__main__":
-    # simulator = Simulator(visualize=True)
-    # simulator.run()
+    if len(sys.argv) != 2:
+        print(f"Usage: {sys.argv[0]} [output directory]\n")
+        sys.exit(1)
 
-    # TODO Get results from args
-    # simulator.output_results("output/test.json")
+    if not os.path.isdir(sys.argv[1]):
+        print(f"Directory {sys.argv[1]} does not exist")
+        sys.exit(1)
 
-    # gen = Generator()
+    timestr = time.strftime("%m%d%Y-%H%M%S")
 
-    # btns = gen.generate()
-    # print(btns)
+    simulator = Simulator(visualize=True)
+    simulator.run()
 
-    # uirepr = UIRepr(btns)
-
-    # print(uirepr)
-
-    # uirepr[13] = 20
-
-    # print()
-    # print(uirepr)
-    # # print(uirepr.matrix())
-    # for i in uirepr:
-    #     print(i)
-
-    # if not os.path.isfile("output/results.csv"):
-
-    ga = GA(results_file="output/results.csv")
-
-    # for i in range(200):
-    # print(f"ga: {list(ga.ga_step(0))}")
-
-    ts = sys.maxsize
-    while (g := ga.ga_step(ts)) is not None:
-        ts = random.random()
-        # print(g)
-
-    ga.output_winner("output/winner.json")
-
-    # t = "text2"
-
-    # print(int(t == "text"))
-
-    # t1 = TestClass(1)
-    # print(t1.x)
-    # t2 = type(t1)(2)
-    # print(t2.x)
-
-    # u = UIRepr(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
-    # print(u)
+    simulator.output_results(os.path.join(sys.argv[1], f"results_{timestr}.json"))
